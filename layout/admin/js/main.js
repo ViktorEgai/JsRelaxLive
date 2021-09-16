ae5742213b8c3939fa4d6e54a786ea8e86014ea5
Egai Viktor <egai.viktor94@gmail.com>
2021-09-17 02:23:36 +0300

    реализация кнопок изменить и удалить

--- layout/admin/js/main.js
+++ layout/admin/js/main.js
@@ -3,16 +3,60 @@ const modal = document.getElementById('modal'),
   form = document.querySelector('form'),
   modalHeader = modal.querySelector('.modal__header'),
   inputs = form.querySelectorAll('input'),
-  saveBtn = document.querySelector('.button-ui_firm');    
-
+  saveBtn = document.querySelector('.button-ui_firm'),
+  addBtn = document.querySelector('.btn-addItem'),
+  closeBtn = document.querySelector('.button__close'),
+  cancelBtn = document.querySelector('.cancel-button');
 // редирект на страницу авторизации если куки пустые
 const redirect = () => {
   if (document.cookie === '') window.location.href = './index.html'
 }
 redirect();
 
+// редактирование списка
+const editData = () => {
+  const tablelist = document.querySelectorAll('.table__row');
+
+  tablelist.forEach(item => {
+    item.addEventListener('click', event => {
+      const target = event.target;
+
+      const  id = item.querySelector('.table__id').textContent,
+        type = item.querySelector('.table-type').textContent,
+        name = item.querySelector('.table-name').textContent,
+        units = item.querySelector('.table-units').textContent,
+        cost = item.querySelector('.table-cost').textContent;   
+      
+      // кнопка изменить
+      if (target.closest('.action-change')) {        
+        openPopup('Редактировать услугу');
+        inputs.forEach((input) => {
+          if (input.id === 'type') input.value = type;
+          if (input.id === 'name') input.value = name;
+          if (input.id === 'units') input.value = units;
+          if (input.id === 'cost') input.value = cost;
+        });
+        form.id = 'editList'; 
+        addNewItem(id);      
+      }    
+
+      // кнопка удалить
+      if (target.closest('.action-remove')) {        
+        fetch('http://localhost:3000/api/items/' + id, {method: 'DELETE'})
+          .then(res =>res.json())
+          .then(res => showTable());        
+      }      
+    })
+  })
+
+};
+
 // прогрузка данных из базы
 const showTable = () => {
+   while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+      };
+
   const getData = () => fetch('http://localhost:3000/api/items');
 
   const getDataTypes = (data) => {
@@ -27,6 +71,10 @@ const showTable = () => {
     const typeSelect = document.getElementById('typeItem'),
       tbody = document.getElementById('tbody');
 
+    // очистка селекта
+     while (typeSelect.firstChild) {
+        typeSelect.removeChild(typeSelect.firstChild);
+      };
     // список селект
     typeArr.forEach(item => {
       typeSelect.insertAdjacentHTML('beforeend', `
@@ -66,6 +114,7 @@ const showTable = () => {
         </tr>
       `);
     }
+
     // данные в таблице
     data.forEach(item => tbodyLayout(item));
 
@@ -77,6 +126,7 @@ const showTable = () => {
       data.forEach(item => {
         if (item.type === typeSelect.value) tbodyLayout(item);
       });
+      editData();
     })
   }
 
@@ -85,64 +135,74 @@ const showTable = () => {
     .then(data => {
       let typeArr = getDataTypes(data);
       showData(data, typeArr);
+      editData();
+
     });
 };
 showTable();
 
 //  открытие/закрытие модального окна
-const togglePopup = (title) => {
-  const addBtn = document.querySelector('.btn-addItem'),
-    closeBtn = document.querySelector('.button__close'),
-    cancelBtn = document.querySelector('.cancel-button');
-
-  //  открытие и закрытие  модального окна
-  addBtn.addEventListener('click', () => {
+const openPopup = (title = 'Добавление новой услуги') => {    
     modalHeader.textContent = title;
     modal.style.display = 'flex';
-  });
-  const closePopup = () => {
-    event.preventDefault();
-    modal.style.display = 'none'
-  }
-  closeBtn.addEventListener('click',  closePopup);
-  cancelBtn.addEventListener('click', closePopup);
 };
-togglePopup('Добавление новой услуги');
+const closePopup = () => {   
+    modal.style.display = 'none'; 
+};
+
+addBtn.addEventListener('click', ()=> {
+  openPopup();
+  form.id = 'newList';
+  inputs.forEach(input => input.value = '');
+  addNewItem();
+});  
+closeBtn.addEventListener('click', closePopup);
+cancelBtn.addEventListener('click', closePopup);
+
+
 
 // добавление нового товара в базу данных
-const addNewItem = () => {  
-  
-  form.addEventListener('submit', () => {      
-    event.preventDefault();      
+const addNewItem = (id) => {  
+  form.addEventListener('submit', () => {    
+    event.preventDefault();
 
-      let body = {};
-      inputs.forEach((input) => {
-        if(input.id === 'type') body['type'] = input.value;
-        if(input.id === 'name') body['name'] = input.value;
-        if(input.id === 'units') body['units'] = input.value;
-        if(input.id === 'cost') body['cost'] = input.value;        
-      });
-      
-      // вызов функции отправки
-      postData(body)
-        .then(resolve => {
-         
-          inputs.forEach(input => input.value = '');
-        })
-        .catch(err => console.error(err))
-    
+    let body = {};
+    inputs.forEach((input) => {
+      if (input.id === 'type') body['type'] = input.value;
+      if (input.id === 'name') body['name'] = input.value;
+      if (input.id === 'units') body['units'] = input.value;
+      if (input.id === 'cost') body['cost'] = input.value;
+    });
+
+    // вызов функции отправки
+    postData(body)
+      .then(resolve => {
+        inputs.forEach(input => input.value = '');
+      })
+      .catch(err => console.error(err))    
+    showTable();
   })
   // функция отправки данных
-  const postData = (body) => {
-    return fetch('http://localhost:3000/api/items', {
+  const postData = (body) => {    
+   if( form.id === 'newList') {
+     return fetch('http://localhost:3000/api/items', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
-    });
+     });  
+   } else if (form.id === 'editList') {
+     return fetch('http://localhost:3000/api/items/' + id, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+     });  
+   }
+      
   };
- 
+
 };
-addNewItem();
 
